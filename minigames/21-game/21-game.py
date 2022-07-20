5e5869405fcd1c92145fb7ad70b7e9f43cc22650
miguelangelo <miguel.juninhoaraujo@gmail.com>
2022-07-20 17:18:33 -0300

    Update 21-game.py

--- minigames/21-game/21-game.py
+++ minigames/21-game/21-game.py
@@ -1,14 +1,25 @@
-import random
+from random import randint
+from sre_constants import BRANCH
 t = 0
-r = input('continuar? ')
-while r == 's':
+c = 0
+while True:
+    pc = randint(1, 10)
+    t = t+pc
+    c = c+1
+    print(f'''
+CARTA: {pc}
+''')
+    if c > 1:
+        print(f'''TOTAL: {t}
+''')
     if t > 21:
-        print('PERDEU')
-    if t == 21:
-        print('GANHOU')
-    num = random.randint(1, 10)
-    t = t+num
-    print(f'carta: {num}')
-    print(f'total: {t}')
-    print('')
-    r = input('continuar? ')
+        print('''PERDEU
+''')
+        break
+    elif t == 21:
+        print('''GANHOU
+''')
+        break
+    resp = input('Deseja continuar? [ENTER/N]')
+    if resp == 'n' or resp == 'N':
+        break
