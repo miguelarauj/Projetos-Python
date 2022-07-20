7ad2628fea5662025c5bd533b18d28122bc1f83d
miguelangelo <miguel.juninhoaraujo@gmail.com>
2022-07-19 22:17:58 -0300

    Update tabuada.py

--- exercises/tabuada.py
+++ exercises/tabuada.py
@@ -1,3 +1,3 @@
-m=int(input('Deseja a tabuada de qual número? '))
-for c in range (1,11):
-    print (f'{m}  x {c:2}= {m*c:2}')
+m = int(input('Deseja a tabuada de qual número? '))
+for c in range(1, 11):
+    print(f'{m}  x {c:2}= {m*c:2}')
