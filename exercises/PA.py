586f4d03422fc47ba50870ac6ae851dee85e84a8
miguelangelo <miguel.juninhoaraujo@gmail.com>
2022-07-25 16:38:10 -0300

    Update PA.py

--- exercises/PA.py
+++ exercises/PA.py
@@ -1,1 +1,9 @@
-termo = int(input('Termo: '))
+termo_inicial = int(input('Termo: '))
+cont_termo = int(input('Quantidade de termos: '))
+razao = int(input('Razão: '))
+decimo = termo_inicial+cont_termo*razao
+for c in range(termo_inicial, decimo, razao):
+    if c == razao*cont_termo+termo_inicial-razao:
+        print(c)
+    else:
+        print(c, ' - ', end=' ')
