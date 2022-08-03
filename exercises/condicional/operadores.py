142db2803ddd2da9e1e2b09c6f694b6f586f7f08
miguelangelo <miguel.juninhoaraujo@gmail.com>
2022-08-03 19:49:44 -0300

    Update

--- exercises/condicional/operadores.py
+++ exercises/condicional/operadores.py
@@ -1,23 +1,3 @@
-"""calclo basico
-"""
-# soma = int(input('escreva 2 + 2'))
-
-# sub = int(input('escreva 2 - 2:'))
-
-# mult = int(input('escreva 2 * 2:'))
-
-# div = int(input('escreva 5 / 2:'))
-
-# pot = int(input('escreva 2 ** 2:'))
-
-# divint = int(input('escreva 5 // 2:'))
-
-# rest = int(input('escreva 3 % 9:'))
-
-# print((5 + 3) * 2)
-# print(5//2)
-# print(5%2)
-
 print('Calculo basico')
 x = int(input('X= '))
 o = str(input('+, -, *, /: '))
