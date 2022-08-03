639a48def5eab6591d8ef615a0afc6636df41352
miguelangelo <miguel.juninhoaraujo@gmail.com>
2022-08-03 17:45:55 -0300

    Update analisador.py

--- exercises/analisador.py
+++ exercises/analisador.py
@@ -1,10 +1,26 @@
 id_media = 0
+cont_h = 0
+cont_m = 0
 for c in range(1, 5):
+    print(f'PESSOA {c}:')
     nome = input('Nome: ')
     idade = int(input('Idade: '))
     sexo = input('Sexo: ').upper()
-    if sexo == 'm':
-        m_velho = nome
+    print()
+    if sexo == 'M':
+        cont_h += 1
+        if cont_h == 1:
+            nome_velho = nome
+            idade_velho = idade
+        elif idade > idade_velho:
+            idade_velho = idade
+            nome_velho = nome
+    elif idade < 20:
+        cont_m += 1
     id_media = id_media+idade
-print(id_media/4)
-print(f'Homem mais velho: {m_velho}')
+print(f'Média de idade: {id_media/4}')
+print(f'Homem mais velho: {nome_velho} tem {idade_velho}')
+if cont_m > 1:
+    print(f'Mulheres com menos de 20: {cont_m}')
+elif cont_m == 1:
+    print('Uma mulher com menos de 20')
