36184f47228817735202f9490614ff58d5e3ee02
miguelangelo <miguel.juninhoaraujo@gmail.com>
2022-08-03 22:01:11 -0300

    Update

--- exercises/repetitiva/while/t1.py
+++ exercises/repetitiva/while/t1.py
@@ -1,11 +1,17 @@
-from os import system
+import os
 from time import sleep
 
-s=0
-m=0
-h=0
-while s<60:
-    s+=1
-    print(f'00:{c}')
+s = 0
+m = 0
+h = 0
+while True:
+    if s < 10:
+        print(f'{m}:0{s}')
+    else:
+        print(f'{m}:{s}')
+    s += 1
     sleep(1)
-    
+    if s >= 59:
+        s = 0
+        m += 1
+    os.system('cls')
