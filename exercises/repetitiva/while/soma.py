a0d40c03769f3c954ae004c3fe3c6ef26799c6e8
miguelangelo <miguel.juninhoaraujo@gmail.com>
2022-08-06 14:14:17 -0300

    Update soma.py

--- exercises/repetitiva/while/soma.py
+++ exercises/repetitiva/while/soma.py
@@ -3,6 +3,6 @@ s = num
 while num != 999:
     print(num, end=' - ')
     print('CONTINUE')
-    num = int(input('Digite um número: '))
     s += num
-print(f'A SOMA DOS VALORES É: {(s-999)}')
+    num = int(input('Digite um número: '))
+print(f'A SOMA DOS VALORES É: {(s)}')
