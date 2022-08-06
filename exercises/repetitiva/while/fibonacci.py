33114ebd50ed151633d466c76c5f81bd6c1ea39e
miguelangelo <miguel.juninhoaraujo@gmail.com>
2022-08-06 13:22:05 -0300

    Update

--- exercises/repetitiva/while/fibonacci.py
+++ exercises/repetitiva/while/fibonacci.py
@@ -1,21 +1,21 @@
-n=int(input('Quantidade de termos: '))
-t1=0
-t2=1
-if n==2:
+n = int(input('Quantidade de termos: '))
+t1 = 0
+t2 = 1
+if n == 2:
     print(t1, end=' - ')
-    print(t2)
-elif n==1:
-    print(t1, end='')
-elif n==0:
+    print(t2, end=' - FIM')
+elif n == 1:
+    print(t1, end=' -  FIM')
+elif n == 0:
     print('FIM')
 else:
-    c=3
+    c = 3
     print(t1, end=' - ')
     print(t2, end=' - ')
-    while c<=n:
-        t3=t1+t2
+    while c <= n:
+        t3 = t1+t2
         print(t3, end=' - ')
-        t1=t2
-        t2=t3
-        c+=1
-print('FIM')
+        t1 = t2
+        t2 = t3
+        c += 1
+    print('FIM')
